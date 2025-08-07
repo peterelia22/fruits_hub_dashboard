e1666b84e56efb56db700689f35ece911b5d9f96
peterelia22 <peterelia914@gmail.com>
2025-08-07 13:45:39 +0300

    feat: Integrate Supabase and add app links and URL launcher plugins for enhanced functionality

--- lib/main.dart
+++ lib/main.dart
@@ -4,6 +4,7 @@ import 'package:cloudinary_url_gen/cloudinary.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:fruits_hub_dashboard/core/services/get_it_service.dart';
+import 'package:supabase_flutter/supabase_flutter.dart';
 
 import 'core/helper_functions/on_generate_routes.dart';
 import 'core/services/custom_bloc_observer.dart';
@@ -12,12 +13,18 @@ import 'firebase_options.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
+  await Supabase.initialize(
+    url: 'https://hucrreuwmvhuvjbavxek.supabase.co',
+    anonKey:
+        'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Imh1Y3JyZXV3bXZodXZqYmF2eGVrIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NTQ1NjAwMzcsImV4cCI6MjA3MDEzNjAzN30.9Rui9nsaiUNp6YuhSeg5Lt20Gl_DqSJMrVioAcSTaK4',
+  );
   Bloc.observer = CustomBlocObserver();
   await Firebase.initializeApp(
     options: DefaultFirebaseOptions.currentPlatform,
   );
   CloudinaryContext.cloudinary =
       Cloudinary.fromCloudName(cloudName: 'dwgw0dluq');
+
   setupGetit();
 
   runApp(const MainApp());
